e896e0fb6eb5363fe349999e3961d18a37394736
=charlie <=475118585@qq.com>
2022-01-19 23:18:37 +0800

    03 把服务注册到consul中

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # learngomicro
+
+go get github.com/micro/micro/v2
+
+go get -u github.com/gin-gonic/gin
+
+go mod download golang.org/x/net
+
+docker run -d --name=cs -p 8500:8500 consul agent -server -bootstrap -ui -client 0.0.0.0
+
+go micro v2 不在默认支持 consul 官方推荐 etcd 
+
+通过 github.com/micro/go-plugins/registry/consul/v2 引入
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/web"
+	"github.com/micro/go-plugins/registry/consul/v2"
 )
 
 func main() {
+	// 新建一个consul注册地址
+	consulReg := consul.NewRegistry(
+		registry.Addrs("localhost:8500"),
+	)
 
 	ginRouter := gin.Default()
 
@@ -18,9 +24,10 @@ func main() {
 	})
 
 	server := web.NewService(
+		web.Name("prodservice"), // 注册到consul服务中的service name
 		web.Address(":8001"),
 		web.Handler(ginRouter),
+		web.Registry(consulReg),  // 注册到哪个服务器上的consul中
 	)
-
 	server.Run()
 }
