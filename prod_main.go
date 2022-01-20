52042a7fb36d6f03023cdd84844205004b1af0a7
=charlie <=475118585@qq.com>
2022-01-20 23:02:03 +0800

    04 模拟准备工作

--- prod_main.go
+++ prod_main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/charlie/micro/ProdServcie"
 	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/web"
@@ -8,20 +9,18 @@ import (
 )
 
 func main() {
-	// 新建一个consul注册地址
+	// 新建_一个consul注册地址
 	consulReg := consul.NewRegistry(
 		registry.Addrs("localhost:8500"),
 	)
 
 	ginRouter := gin.Default()
-
-	ginRouter.Handle("GET", "/user", func(context *gin.Context) {
-		context.String(200, "user api")
-	})
-
-	ginRouter.Handle("GET", "/news", func(context *gin.Context) {
-		context.String(200, "new api")
-	})
+	v1Group := ginRouter.Group("/v1")
+	{
+		v1Group.Handle("GET", "/prods", func(context *gin.Context) {
+			context.JSON(200,ProdServcie.NewProdList(5))
+		})
+	}
 
 	server := web.NewService(
 		web.Name("prodservice"), // 注册到consul服务中的service name
